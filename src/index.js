95f1385eca6209cb75e620602cacdcd97e9c9224
hobada96 <hobada96@gmail.com>
2021-01-01 20:27:28 +0900

    랜덤으로 3개 뽑기 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 export default function BaseballGame() {
-  this.play = function (computerInputNumbers, userInputNumbers) {
-    return "결과 값 String";
-  };
+    this.play = function (computerInputNumbers, userInputNumbers) {
+        return userInputNumbers;
+    };
+}
+const makeRandomNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+const makeComputerInputNumbers = (repeat) => {
+  const computerInputNumbers = [];
+    while(computerInputNumbers.length != 3) {
+      const randomNum = makeRandomNumber(1,10);
+      !computerInputNumbers.some((number) => number === randomNum) && computerInputNumbers.push(randomNum)
+    }
+    alert(computerInputNumbers)
 }
 
-// export default class BaseballGame {
-//   play(computerInputNumbers, userInputNumbers) {
-//     return "결과 값 String";
-//   }
-// }
-
-new BaseballGame();
+makeComputerInputNumbers(3);
